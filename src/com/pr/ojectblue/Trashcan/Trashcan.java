6fd7407d3be189930dea5f7bff66f02aac09daa6
Jason Stallings <jason@stallin.gs>
2017-12-26 11:23:09 -0600

    Updated for 1.12.2

--- src/com/pr/ojectblue/Trashcan/Trashcan.java
+++ src/com/pr/ojectblue/Trashcan/Trashcan.java
@@ -1,6 +1,5 @@
 package com.pr.ojectblue.Trashcan;
 
-
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -10,53 +9,50 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.InventoryView;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
+public class Trashcan extends JavaPlugin implements Listener {
 
-
-public class Trashcan extends JavaPlugin implements Listener{
-	
-	
-	public void onEnable(){
-		getLogger().info("The Trashcan 0.3 has been enabled!");
+	public void onEnable() {
+		getLogger().info("The Trashcan 0.1.0 has been enabled!");
 		getServer().getPluginManager().registerEvents(this, this);
-		
+
 		/*
-		Creates the recipe to be trashcan shaped.
-		o   o
-		o   o
-		o o o
-		*/
-		ShapedRecipe RECIPE = new ShapedRecipe(new ItemStack(Material.CAULDRON_ITEM,1)).shape("o o", "o o", "ooo").setIngredient('o', Material.COBBLESTONE);
-		
-		//Adds the recipe to the server. This is important.
+		 * Creates the recipe to be trashcan shaped. o o o o o o o
+		 */
+		// TODO: Update to use new ShapedRecipe API with namespace. See: https://www.spigotmc.org/threads/1-12-shapedrecipe-namespacedkey.240616/
+		ShapedRecipe RECIPE = new ShapedRecipe(new ItemStack(Material.CAULDRON_ITEM, 1)).shape("o o", "o o", "ooo")
+				.setIngredient('o', Material.COBBLESTONE);
+
+		// Adds the recipe to the server. This is important.
 		getServer().addRecipe(RECIPE);
-		
-	}
- 
-	public void onDisable(){
-		getLogger().info("The Trashcan 0.3 has been disabled.");
+
 	}
 
-	
-	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled=true)
-    public void onPlayerInteract(PlayerInteractEvent event) {
-        Player player = event.getPlayer();
-        //When the user right clicks a block.
-        if(event.getAction() == Action.RIGHT_CLICK_BLOCK)
-        {
-        	//Check to see if they clicked a cauldron, and if they did make sure they aren't holding a water bucket or a glass bottle.
-        	//Thanks to Algent for coming up with a simple way to make the Cauldron still usable. 
-            if (event.getClickedBlock().getType() == Material.CAULDRON && player.getItemInHand().getType() != Material.WATER_BUCKET && player.getItemInHand().getType() != Material.GLASS_BOTTLE && player.hasPermission("thetrashcan.use")) 
-            {
-            	//Opens the trashcan gui as an inventory.
-            	Inventory inventory = Bukkit.createInventory(player, 36, "Trash Can");              
-                player.openInventory(inventory);
+	public void onDisable() {
+		getLogger().info("The Trashcan 0.1.0 has been disabled.");
+	}
 
-            }
-        }
+	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+	public void onPlayerInteract(PlayerInteractEvent event) {
+		Player player = event.getPlayer();
+		// When the user right clicks a block.
+		if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+			// Check to see if they clicked a cauldron, and if they did make sure they
+			// aren't holding a water bucket or a glass bottle.
+			// Thanks to Algent for coming up with a simple way to make the Cauldron still
+			// usable.
+			if (event.getClickedBlock().getType() == Material.CAULDRON
+					&& player.getInventory().getItemInMainHand().getType() != Material.WATER_BUCKET
+					&& player.getInventory().getItemInMainHand().getType() != Material.GLASS_BOTTLE
+					&& player.hasPermission("thetrashcan.use")) {
+				// Opens the trashcan gui as an inventory.
+				Inventory inventory = Bukkit.createInventory(player, 36, "Trash Can");
+				player.openInventory(inventory);
+
+			}
+		}
 	}
 }
